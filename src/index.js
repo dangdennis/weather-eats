c6e15de51d6ce516582f05c382d1c67e1d2ecb31
Dennis Dang <dangggdennis@gmail.com>
2017-08-04 12:11:27 -0700

    with redux applied

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import rootReducer from './reducers/index';
 
 import App from './components/app';
 
+const store = applyMiddleware(createStore);
+
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+	<Provider store={store(rootReducer)}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
 );
